d6cb5e448a5a8d9e609b4b61c5bbbe233821559c
Eric Tedor <eric@tedor.org>
2023-09-02 14:06:52 -0700

    add reverse proxy

--- hosts/servers/code/configuration.nix
+++ hosts/servers/code/configuration.nix
@@ -1,5 +1,8 @@
 { config, lib, pkgs, ... }:
 
+let
+  zone = "et42.net";
+in
 {
   imports = [
     ./hardware-configuration.nix
@@ -42,5 +45,95 @@
     NIX_LD = lib.mkForce "${pkgs.stdenv.cc.bintools.dynamicLinker}";
   };
 
-  networking.firewall.allowedTCPPorts = [ 8080 19999 50080 ];
+  age.secrets = {
+    code-acme = {
+      file = ../../../secrets/code-acme.age;
+      mode = "400";
+      owner = "acme";
+      group = "acme";
+    };
+  };
+
+  security.acme = {
+    acceptTerms = true;
+    defaults.email = "admin@${zone}";
+
+    certs."${zone}" = {
+      domain = "${zone}";
+      extraDomainNames = [ "*.${zone}" ];
+      dnsProvider = "cloudflare";
+      dnsPropagationCheck = true;
+      credentialsFile = config.age.secrets.code-acme.path;
+    };
+  };
+
+  users.users.nginx.extraGroups = [ "acme" ];
+  services.nginx =
+    let
+      defaults = {
+        extraConfig = ''
+          proxy_buffering off;
+        '';
+        forceSSL = true;
+        useACMEHost = zone;
+      };
+
+      pduFavicon = {
+        proxyPass = "https://www.apc.com/favicon.ico";
+        recommendedProxySettings = false;
+        extraConfig = ''
+          proxy_set_header Referer "";
+          proxy_set_header Origin "";
+        '';
+      };
+    in
+    {
+      enable = true;
+
+      recommendedGzipSettings = true;
+      recommendedOptimisation = true;
+      recommendedProxySettings = true;
+      recommendedTlsSettings = true;
+
+      virtualHosts = {
+        "_" = { locations."/" = { return = "404"; }; };
+
+        "ha.${zone}" = ({
+          locations."/" = {
+            proxyPass = "http://10.0.11.11:8123";
+            proxyWebsockets = true;
+          };
+        } // defaults);
+
+        "nr.${zone}" = ({
+          locations."/" = {
+            proxyPass = "http://10.0.11.11:1880";
+            proxyWebsockets = true;
+          };
+        } // defaults);
+
+        "og.${zone}" = ({
+          locations."/".proxyPass = "https://10.0.2.253:443";
+        } // defaults);
+
+        "pdu1.${zone}" = ({
+          locations."/".proxyPass = "http://10.0.2.12:80";
+          locations."/favicon.ico" = pduFavicon;
+        } // defaults);
+        "pdu2.${zone}" = ({
+          locations."/".proxyPass = "http://10.0.2.13:80";
+          locations."/favicon.ico" = pduFavicon;
+        } // defaults);
+        "pdu3.${zone}" = ({
+          locations."/".proxyPass = "http://10.0.2.14:80";
+          locations."/favicon.ico" = pduFavicon;
+        } // defaults);
+
+        "ups.${zone}" = ({
+          locations."/".proxyPass = "http://10.0.2.11:80";
+        } // defaults);
+      };
+    };
+
+  networking.firewall.allowedTCPPorts = [ 80 443 8080 19999 50080 ];
 }
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,14 +1,15 @@
 let
   # user keys
   eric_machina = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICgNd8ZgyJiay+vUZxvOzXqNsbmjhqzwFZx1U3+LnAVz";
-  eric_nix-vscode = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAJ9Ya8PpfcuSHtX52hJUXMDFJwUUyJR+0s6FuSTfEKn";
-  eric = [ eric_machina eric_nix-vscode ];
+  eric_code = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAJ9Ya8PpfcuSHtX52hJUXMDFJwUUyJR+0s6FuSTfEKn";
+  eric = [ eric_machina eric_code ];
 
   # host keys
-  nix-vscode = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFVyPMj/Xq3Nxha/vj1S9xCaEWQMcjsweLpMeDDFjzmN";
+  code = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFVyPMj/Xq3Nxha/vj1S9xCaEWQMcjsweLpMeDDFjzmN";
   rt-sea = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHqcrgjHPx1SbllfVSCLcj/g29HAW/qcv6i6ZYoNs99h";
 in
 {
+  "code-acme.age".publicKeys = eric ++ [ code ];
   "rt-sea-wg0.age".publicKeys = eric ++ [ rt-sea ];
   "rt-sea-wg1.age".publicKeys = eric ++ [ rt-sea ];
 }
