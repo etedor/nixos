acd9224229fb643d4dbd386681da66f23a841b05
Eric Tedor <eric@tedor.org>
2023-09-09 17:03:21 -0700

    add unstable

--- flake.nix
+++ flake.nix
@@ -1,36 +1,36 @@
 {
   description = "My NixOS configuration";
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+
     agenix.url = "github:ryantm/agenix";
   };
 
-  outputs = { self, nixpkgs, agenix }: {
-    nixosConfigurations = {
+  outputs = { self, nixpkgs, nixpkgs-unstable, agenix }: {
+    nixosConfigurations =
+      let
+        overlay-unstable = final: prev: {
+          unstable = nixpkgs-unstable.legacyPackages.${prev.system};
+        };
 
-      code = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
+        commonModules = [
           ./common
-          ./hosts/servers/code/configuration.nix
-
+          ({ config, pkgs, ... }: { nixpkgs.overlays = [ overlay-unstable ]; })
           agenix.nixosModules.default
           { environment.systemPackages = [ agenix.packages.x86_64-linux.default ]; }
         ];
+      in
+      {
+        code = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = commonModules ++ [ ./hosts/servers/code/configuration.nix ];
+        };
+
+        rt-sea = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = commonModules ++ [ ./hosts/routers/rt-sea/configuration.nix ];
+        };
       };
-
-      rt-sea = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./common
-          ./hosts/routers/rt-sea/configuration.nix
-
-          agenix.nixosModules.default
-          { environment.systemPackages = [ agenix.packages.x86_64-linux.default ]; }
-        ];
-      };
-
-    };
   };
 }
