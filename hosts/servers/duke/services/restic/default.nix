a5d2eec0ab1a1a7c1c5a53031792ebaa8a3a3595
Eric Tedor <eric@tedor.org>
2023-09-25 20:01:55 -0700

    add docker volumes

--- hosts/servers/duke/services/restic/default.nix
+++ hosts/servers/duke/services/restic/default.nix
@@ -25,6 +25,14 @@
   services.restic.backups =
     let
       dirs = [
+        {
+          path = "/pool0/docker/volumes";
+          excludes = [
+            "media_jellyfin_config/_data/data/metadata"
+            "media_radarr_config/_data/MediaCover"
+            "media_sonarr_config/_data/MediaCover"
+          ];
+        }
         { path = "/pool0/users/eric"; excludes = [ "**/.git" ]; }
       ];
 
@@ -55,8 +63,5 @@
         };
       };
     };
-
-
-
 
 }
